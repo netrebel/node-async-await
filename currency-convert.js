1d49c37746abb280ac4189c997f4b15d431c6df3
Miguel Reyes <miguel.reyes@enernoc.com>
2018-09-20 16:20:26 -0400

    Handling Errors

--- currency-convert.js
+++ currency-convert.js
@@ -5,15 +5,28 @@ const axios = require('axios');
 // http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136
 
 const getExchangeRate = async (from, to) => {
-    const response = await axios.get('http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136');
-    const euro = 1 / response.data.rates[from];
-    const rate = euro * response.data.rates[to];
-    return rate;
+    try {
+        const response = await axios.get('http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136');
+        const euro = 1 / response.data.rates[from];
+        const rate = euro * response.data.rates[to];
+
+        if (isNaN(rate)) {
+            throw new Error();
+        }
+
+        return rate;
+    } catch (e) {
+        throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+    }
 };
 
 const getCountries = async (currencyCode) => {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-    return response.data.map((country) => country.name);    
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+        return response.data.map((country) => country.name);    
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}.`);
+    }
 }
 
 const convertCurrency = async (from, to, amount) => {
@@ -24,4 +37,6 @@ const convertCurrency = async (from, to, amount) => {
 
 convertCurrency('USD', 'CAD', 100).then((message) => {
     console.log(message);
+}).catch((e) => {
+    console.log(e.message);
 });
