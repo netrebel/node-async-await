e05b77cdccd219fcf16327fe2d3619c05ae33435
Miguel Reyes <miguel.reyes@enernoc.com>
2018-09-20 16:04:25 -0400

    Async/await with axios http request - final

--- currency-convert.js
+++ currency-convert.js
@@ -1,15 +1,8 @@
 // USD, CAD, 20
+// 20 UDS is worth 24 in CAD. You can spend these in the following countries: Canada.
 const axios = require('axios');
 
 // http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136
-
-// const getExchangeRate = (from, to) => {
-//     return axios.get('http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136').then((response) => {
-//         const euro = 1 / response.data.rates[from];
-//         const rate = euro * response.data.rates[to];
-//         return rate;
-//     });
-// };
 
 const getExchangeRate = async (from, to) => {
     const response = await axios.get('http://data.fixer.io/api/latest?access_key=a6ac9dd5adf7837f567d6224bee40136');
@@ -18,22 +11,17 @@ const getExchangeRate = async (from, to) => {
     return rate;
 };
 
-// const getCountries = (currencyCode) => {
-//     return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
-//         return response.data.map((country) => country.name)
-//     });
-// }
-
 const getCountries = async (currencyCode) => {
     const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map((country) => country.name);    
 }
 
-getExchangeRate('USD', 'CAD').then((rate) => {
-    console.log(rate);
-    
-});
+const convertCurrency = async (from, to, amount) => {
+    const rate = await getExchangeRate(from, to);
+    const countries = await getCountries(to);
+    return `${amount} ${from} is worth ${(amount * rate).toFixed(2)} ${to}. You can spend these in the following countries: ${countries.join(', ')}`;
+}
 
-getCountries('CAD').then((countries) => {
-    console.log(countries);
+convertCurrency('USD', 'CAD', 100).then((message) => {
+    console.log(message);
 });
